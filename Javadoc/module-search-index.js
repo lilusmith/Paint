46a917c23ab89ca95088cab9c0ed8b0e1dec4c0e
lilusmith <122748319+lilusmith@users.noreply.github.com>
2023-11-25 02:05:42 -0500

    Add files via upload

---
+++ Javadoc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"com.example.paint1"}];updateSearchResults();
---
+++ Javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.example.paint1","l":"com.example.paint1"}];updateSearchResults();
